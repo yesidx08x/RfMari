789f7f9a7ed4f2a737c4562f0f6128c9c322c275
Axe King <yes_id@163.com>
2017-04-25 18:39:40 +0800

    Create register_custom_shaders.py

---
+++ register_custom_shaders.py
@@ -0,0 +1,2 @@
+mari.gl_render.registerCustomStandaloneShaderFromXMLFile("PxrBXDF","X:/TD/Mari_Scripts/Custom_Shaders/PxrBXDF.xml")
+mari.gl_render.registerCustomStandaloneShaderFromXMLFile("MantraSrf","X:/TD/Mari_Scripts/Custom_Shaders/MantraSrf.xml")
